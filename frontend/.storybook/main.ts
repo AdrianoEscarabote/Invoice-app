e5ca9644a476b95a1ef4e5f5959a45e20126d5d6
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-11-01 12:56:01 -0300

    feat(storybook): setup storybook and dowloading @storybook/addons

---
+++ frontend/.storybook/main.ts
@@ -0,0 +1,24 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  core: {},
+  staticDirs: ["../public"],
+  typescript: {
+    reactDocgen: false,
+  },
+};
+
+export default config;
