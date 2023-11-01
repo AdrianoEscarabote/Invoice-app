9a97f77ab0c47987dc0d11bba4cc9b0f841cefe6
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-11-01 13:33:45 -0300

    chore(frontend): configuring the font-family and updating favicon

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import "./styles/global.css";
+import Head from "next/head";
+
+export const metadata: Metadata = {
+  title: "Invoice web app",
+  description:
+    "A versatile invoicing application that allows users to seamlessly manage invoices. ",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=7" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>{children}</body>
+    </html>
+  );
+}
