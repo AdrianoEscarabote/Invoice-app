8ec25896f277abea6ee548493c6eaa65d9c86cc9
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-11-06 21:39:43 -0300

    feat(components): adding edit button component

---
+++ frontend/src/app/components/EditButton/EditButton.test.tsx
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import EditButton from ".";
+
+describe("EditButton", () => {
+  it("should render correctly", () => {
+    render(<EditButton />);
+  });
+});
