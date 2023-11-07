274705401079eb3f686c097ad904ec481845cede
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-11-07 11:25:01 -0300

    feat(components): adding draft button component

---
+++ frontend/src/app/components/DraftButton/DraftButton.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, StoryObj } from "@storybook/react";
+import DraftButton from "./";
+
+export default {
+  title: "components/DraftButton",
+  component: DraftButton,
+  tags: ["autodocs"],
+} as Meta;
+
+export const Primary: StoryObj = {};
---
+++ frontend/src/app/components/DraftButton/DraftButton.test.tsx
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import DraftButton from ".";
+
+describe("DraftButton", () => {
+  it("should render correctly", () => {
+    render(<DraftButton />);
+  });
+});
