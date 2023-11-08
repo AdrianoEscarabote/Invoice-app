43e91f2caae68b51e17525e019c5ec3839d44b01
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-11-08 11:26:28 -0300

    feat(components): adding toggle theme button component

---
+++ frontend/src/app/components/ToggleThemeBtn/ToggleThemeBtn.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from "@storybook/react";
+import ToggleThemeBtn from ".";
+
+export default {
+  title: "components/ToggleThemeBtn",
+  component: ToggleThemeBtn,
+} as Meta;
+
+export const Primary: StoryObj = {};
