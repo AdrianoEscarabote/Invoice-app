8729fe47ab513ce91937b70c7d130b2849fff7ec
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-12-10 14:39:29 -0300

    chore(components): create initial structure for delte prompt component

---
+++ frontend/src/components/DeletePrompt/DeletePrompt.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from "@storybook/react";
+import DeletePrompt from ".";
+
+export default {
+  title: "components/DeletePrompt",
+  component: DeletePrompt,
+} as Meta;
+
+export const Primary: StoryObj = {};
