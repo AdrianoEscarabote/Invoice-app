ef444b1cc360f4efec5243fc6548dcc3c2a9f2b7
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-12-21 16:48:41 -0300

    feat(components): add save button component

---
+++ frontend/src/components/SaveButton/SaveButton.test.tsx
@@ -0,0 +1,19 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import SaveButton from ".";
+
+describe("SaveButton", () => {
+  it("should render correctly", () => {
+    render(<SaveButton />);
+  });
+
+  it("should call the function passed as prop correctly", () => {
+    const mockFn = jest.fn();
+    render(<SaveButton SaveFn={mockFn} />);
+
+    const button = screen.getByText("Save Changes");
+
+    fireEvent.click(button);
+
+    expect(mockFn).toHaveBeenCalled();
+  });
+});
