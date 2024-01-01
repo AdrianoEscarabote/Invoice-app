093550d3db6b85c6d4105e7cb78c6b301ef9d090
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-01-01 20:10:08 -0300

    style(components): changing max-w to min-w

--- frontend/src/components/InvoiceStatus/index.tsx
+++ frontend/src/components/InvoiceStatus/index.tsx
@@ -5,7 +5,7 @@ const InvoiceStatus = ({ status }: InvoiceStatusProps) => {
   return (
     <>
       {status === "Paid" && (
-        <p className="text-[#33D69F] HeadingSVariant relative h-10 w-full max-w-[104px] rounded-lg bg-[#33D69F] bg-opacity-5 gap-2 flex items-center justify-center">
+        <p className="text-[#33D69F] HeadingSVariant relative h-10 w-full min-w-[104px] rounded-lg bg-[#33D69F] bg-opacity-5 gap-2 flex items-center justify-center">
           <span className={`bg-[#33d69f] ${style.circle}`}></span>
           Paid
         </p>
@@ -13,14 +13,14 @@ const InvoiceStatus = ({ status }: InvoiceStatusProps) => {
       {status === "Pending" && (
         <p
           data-testid="pendingButton"
-          className="text-[#FF8F00] HeadingSVariant relative h-10 w-full max-w-[104px] rounded-lg bg-[#FF8F00] bg-opacity-5 flex items-center justify-center gap-2"
+          className="text-[#FF8F00] HeadingSVariant relative h-10 w-full min-w-[104px] rounded-lg bg-[#FF8F00] bg-opacity-5 flex items-center justify-center gap-2"
         >
           <span className={`bg-[#FF8F00] ${style.circle}`}></span>
           Pending
         </p>
       )}
       {status === "Draft" && (
-        <p className="status-draft-color HeadingSVariant relative h-10 w-full max-w-[104px] rounded-lg bg-[#DFE3FA] bg-opacity-5 flex items-center justify-center gap-2">
+        <p className="status-draft-color HeadingSVariant relative h-10 w-full min-w-[104px] rounded-lg bg-[#DFE3FA] bg-opacity-5 flex items-center justify-center gap-2">
           <span className={`bg-[#373B53] ${style.circle}`}></span>
           Draft
         </p>
