d356dd48d6a26cf15d97fb1eafaf2edd24ccc8b5
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-01-04 16:47:46 -0300

    feat(components): add DeleteButtonProps interface

--- frontend/src/components/DeleteButton/index.tsx
+++ frontend/src/components/DeleteButton/index.tsx
@@ -1,6 +1,11 @@
-const DeleteButton = () => {
+import { DeleteButtonProps } from "./DeleteButtonProps";
+
+const DeleteButton = ({ onClick }: DeleteButtonProps) => {
   return (
-    <button className="HeadingSVariant w-full max-w-[89px] h-12 rounded-3xl text-white bg-red hover:bg-light_red">
+    <button
+      onClick={onClick}
+      className="HeadingSVariant w-full max-w-[89px] h-12 rounded-3xl text-white bg-red hover:bg-light_red"
+    >
       Delete
     </button>
   );
