a39c2d65456a855f9bbbb3876253123194dc459a
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-01-04 16:56:50 -0300

    test(components): add new unit tests

--- frontend/src/components/DeleteButton/DeleteButton.test.tsx
+++ frontend/src/components/DeleteButton/DeleteButton.test.tsx
@@ -1,8 +1,20 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import DeleteButton from ".";
 
 describe("DeleteButton", () => {
   it("should render correctly", () => {
-    render(<DeleteButton />);
+    render(<DeleteButton onClick={() => {}} />);
+  });
+
+  it("should call the onclick prop when the button is clicked", () => {
+    const mockFn = jest.fn();
+
+    render(<DeleteButton onClick={mockFn} />);
+
+    const button = screen.getByText("Delete");
+
+    fireEvent.click(button);
+
+    expect(mockFn).toHaveBeenCalled();
   });
 });
--- frontend/src/components/PaidButton/PaidButton.test.tsx
+++ frontend/src/components/PaidButton/PaidButton.test.tsx
@@ -1,8 +1,20 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import PaidButton from ".";
 
 describe("PaidButton", () => {
   it("should render correctly", () => {
-    render(<PaidButton />);
+    render(<PaidButton onClick={() => {}} />);
+  });
+
+  it("should call the onclick prop when the button is clicked", () => {
+    const mockFn = jest.fn();
+
+    render(<PaidButton onClick={mockFn} />);
+
+    const button = screen.getByText("Mark as Paid");
+
+    fireEvent.click(button);
+
+    expect(mockFn).toHaveBeenCalled();
   });
 });
