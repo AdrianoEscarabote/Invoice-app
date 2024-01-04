13309d7cac540cf4a14669e363fea55d06723a3f
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-01-04 17:18:58 -0300

    feat(components): add PaidButtonProps interface

---
+++ frontend/src/components/PaidButton/PaidButtonProps.d.ts
@@ -0,0 +1,3 @@
+export interface PaidButtonProps {
+  onClick: () => void;
+}
--- frontend/src/components/PaidButton/index.tsx
+++ frontend/src/components/PaidButton/index.tsx
@@ -1,6 +1,11 @@
-const PaidButton = () => {
+import { PaidButtonProps } from "./PaidButtonProps";
+
+const PaidButton = ({ onClick }: PaidButtonProps) => {
   return (
-    <button className="HeadingSVariant bg-dark_purple text-white rounded-3xl w-full max-w-[131px] h-12 hover:bg-light_purple">
+    <button
+      onClick={onClick}
+      className="HeadingSVariant bg-dark_purple text-white rounded-3xl w-full max-w-[131px] h-12 hover:bg-light_purple"
+    >
       Mark as Paid
     </button>
   );
