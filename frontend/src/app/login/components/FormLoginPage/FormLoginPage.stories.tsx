d285027cc4a9c614027890a85f6a3f45d96ce79e
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-01-07 18:54:12 -0300

    feat(components): add form login page

---
+++ frontend/src/app/login/components/FormLoginPage/FormLoginPage.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from "@storybook/react";
+import FormLoginPage from ".";
+
+export default {
+  title: "login/components/FormLoginPage",
+  component: FormLoginPage,
+} as Meta;
+
+export const Primary: StoryObj = {};
---
+++ frontend/src/app/login/components/FormLoginPage/FormLoginPage.test.tsx
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import FormLoginPage from ".";
+
+describe("FormLoginPage", () => {
+  it("should render correctly", () => {
+    render(<FormLoginPage />);
+  });
+});
