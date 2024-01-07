c611d5facffee133f1062b80ea955f4005531e9b
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-01-06 21:46:14 -0300

    feat(login): add login stories

---
+++ frontend/src/app/login/login.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from "@storybook/react";
+import LoginPage from "./page";
+
+export default {
+  title: "login",
+  component: LoginPage,
+} as Meta;
+
+export const Primary: StoryObj = {};
