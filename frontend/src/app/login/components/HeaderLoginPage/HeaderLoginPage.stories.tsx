7725af71e599baed19b7d44d64dcad1981b6a620
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-01-08 11:54:06 -0300

    chore(login): deleting header component and fixing imports

--- frontend/src/app/login/components/HeaderLoginPage/HeaderLoginPage.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Meta, StoryObj } from "@storybook/react";
-import HeaderLoginPage from ".";
-
-export default {
-  title: "login/components/HeaderLoginPage",
-  component: HeaderLoginPage,
-} as Meta;
-
-export const Primary: StoryObj = {};
