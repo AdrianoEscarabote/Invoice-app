183066c7436b51f894e9d8c06656c6b006833b28
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-01-08 12:09:55 -0300

    feat(components): add SignupForm component

---
+++ frontend/src/app/signup/components/SignupForm/SignupForm.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from "@storybook/react";
+import SignupForm from ".";
+
+export default {
+  title: "signup/components/SignupForm",
+  component: SignupForm,
+} as Meta;
+
+export const Primary: StoryObj = {};
