8f3700d24a8fffd6488f6a4a99b1d84ebb203b77
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-01-10 13:40:02 -0300

    feat(components): adding status button props and refactoring

--- frontend/src/components/StatusButton/index.tsx
+++ frontend/src/components/StatusButton/index.tsx
@@ -1,70 +1,104 @@
 import Image from "next/image";
 import { useState } from "react";
 import style from "./style.module.css";
+import HeaderLoginSignup from "../HeaderLoginSignup";
+import { statusType, StatusButtonProps } from "./StatusButtonProps";
 
-const StatusButton = () => {
+const StatusButton = ({ statusProp }: StatusButtonProps) => {
   const [optionsOpen, setOptionsOpen] = useState<boolean>(false);
+  const [status, setStatus] = useState<statusType>(statusProp);
 
   return (
-    <div className="flex flex-col relative HeadingSVariant">
-      <button
-        className="flex items-center gap-3 w-full min-w-[132px] py-3"
-        onClick={() => setOptionsOpen(!optionsOpen)}
-      >
-        Filter by Status
-        <Image
-          src={"/assets/icon-arrow-down.svg"}
-          alt="icon arrow"
-          width={10}
-          height={10}
-        />
-      </button>
-      {optionsOpen ? (
-        <div className="w-[192px] p-6 absolute top-8 flex flex-col gap-2 rounded-lg">
-          <label
-            className={`flex items-center gap-3 ${style.container}`}
-            htmlFor="draft"
-          >
-            Draft
-            <input
-              className={`${style.input}`}
-              type="checkbox"
-              name="draft"
-              id="draft"
-            />
-            <span className={`${style.checkmark}`}></span>
-          </label>
+    <>
+      <HeaderLoginSignup />
+      <div className="flex flex-col relative HeadingSVariant text-color2">
+        <button
+          className="flex items-center gap-[14px] w-full min-w-[136px] py-3"
+          onClick={() => setOptionsOpen(!optionsOpen)}
+        >
+          Filter by Status
+          <Image
+            src={"/assets/icon-arrow-down.svg"}
+            alt="icon arrow"
+            width={10}
+            style={
+              optionsOpen
+                ? {
+                    transform: "rotate(180deg)",
+                  }
+                : {}
+            }
+            height={10}
+          />
+        </button>
+        {optionsOpen ? (
+          <div className="w-[192px] p-6 absolute top-11 -right-7 flex flex-col gap-4  rounded-lg bg-3 z-50 status_shadow">
+            <label
+              className={`flex items-center gap-3 HeadingSVariant ${style.container}`}
+              htmlFor="draft"
+            >
+              <span className="mt-1">Draft</span>
+              <input
+                className={`${style.input}`}
+                type="checkbox"
+                name="draft"
+                id="draft"
+                onClick={() => setStatus("Draft")}
+              />
+              <span
+                className={`${style.checkmark} bg-status ${
+                  status === "Draft" ? style.checked : ""
+                } bg-2`}
+              >
+                <div className={`${style.correct}`}>&#x2713;</div>
+              </span>
+            </label>
 
-          <label
-            className={`flex items-center gap-3 ${style.container}`}
-            htmlFor="pending"
-          >
-            Pending
-            <input
-              className={`${style.input}`}
-              type="checkbox"
-              name="pending"
-              id="pending"
-            />
-            <span className={`${style.checkmark}`}></span>
-          </label>
+            <label
+              className={`flex items-center HeadingSVariant gap-3 ${style.container}`}
+              htmlFor="pending"
+            >
+              <span className="mt-1">Pending</span>
+              <input
+                className={`${style.input}`}
+                type="checkbox"
+                name="pending"
+                id="pending"
+                onClick={() => setStatus("Pending")}
+              />
+              <span
+                className={`${style.checkmark} bg-status ${
+                  status === "Pending" ? style.checked : ""
+                } bg-2`}
+              >
+                <div className={`${style.correct}`}>&#x2713;</div>
+              </span>
+            </label>
 
-          <label
-            className={`flex items-center gap-3 ${style.container}`}
-            htmlFor="paid"
-          >
-            Paid
-            <input
-              className={`${style.input}`}
-              type="checkbox"
-              name="draft"
-              id="paid"
-            />
-            <span className={`${style.checkmark}`}></span>
-          </label>
-        </div>
-      ) : null}
-    </div>
+            <label
+              className={`flex items-center HeadingSVariant gap-3 ${style.container}`}
+              htmlFor="paid"
+            >
+              <span className="mt-1">Paid</span>
+              <input
+                className={`${style.input}`}
+                type="checkbox"
+                name="draft"
+                id="paid"
+                onClick={() => setStatus("Paid")}
+              />
+              <span
+                className={`${style.checkmark} bg-status ${
+                  status === "Paid" ? style.checked : ""
+                } bg-2`}
+              >
+                <div className={`${style.correct}`}>&#x2713;</div>
+              </span>
+            </label>
+          </div>
+        ) : null}
+      </div>
+    </>
   );
 };
 
