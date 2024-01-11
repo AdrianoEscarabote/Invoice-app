b872b4766f897f0aaff4a28c0956defdcd9f6714
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-01-11 17:43:28 -0300

    feat(components): adding new interface, tests and refactoring the entire component

--- frontend/src/components/NewItemButton/NewItemButton.test.tsx
+++ frontend/src/components/NewItemButton/NewItemButton.test.tsx
@@ -1,8 +1,23 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import NewItemButton from ".";
 
 describe("NewItemButton", () => {
-  it("should render correctly", () => {
-    render(<NewItemButton />);
+  it("should render correctly with props being passed", () => {
+    const onClickFn = jest.fn();
+
+    render(
+      <NewItemButton
+        disabled={false}
+        type="button"
+        data-testid="button-id"
+        onClick={onClickFn}
+      />
+    );
+
+    const button = screen.getByTestId("button-id");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveAttribute("type");
+    fireEvent.click(button);
+    expect(onClickFn).toHaveBeenCalled();
   });
 });
