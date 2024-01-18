bc986ab96977243079efc29eb30200a510b5e4ec
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-01-18 13:24:53 -0300

    chore(components): configuring storybook file

--- frontend/src/app/signup/components/SignupForm/SignupForm.stories.tsx
+++ frontend/src/app/signup/components/SignupForm/SignupForm.stories.tsx
@@ -4,6 +4,13 @@ import SignupForm from ".";
 export default {
   title: "signup/components/SignupForm",
   component: SignupForm,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "fullscreen",
+    nextjs: {
+      appDirectory: true,
+    },
+  },
 } as Meta;
 
 export const Primary: StoryObj = {};
