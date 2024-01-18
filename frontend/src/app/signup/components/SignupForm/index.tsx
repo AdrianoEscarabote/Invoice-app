2dd9cd02fd06296abceb67d6902fae02e5daaefe
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-01-18 13:22:01 -0300

    feat(components): adding checktoken request

--- frontend/src/app/signup/components/SignupForm/index.tsx
+++ frontend/src/app/signup/components/SignupForm/index.tsx
@@ -3,11 +3,15 @@
 import InputForm from "@/components/InputForm";
 import style from "./style.module.css";
 import ButtonForm from "@/components/ButtonForm";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { SignupFormProps } from "./SignupFormProps";
+import { useRouter } from "next/navigation";
 
 const SignupForm = () => {
+  const url = process.env.NEXT_PUBLIC_API_URL;
+  const router = useRouter();
+  const [errorData, setErrorData] = useState<string>("");
   const [showLoadingComponent, setShowLoadingComponent] =
     useState<boolean>(false);
   const {
@@ -17,16 +21,55 @@ const SignupForm = () => {
     formState: { errors },
   } = useForm<SignupFormProps>();
 
-  const onSubmit = handleSubmit(async (data) => {
-    alert("test!");
+  useEffect(() => {
+    const checkToken = async () => {
+      const response = await fetch(`${url}/auth/checktoken`, {
+        method: "GET",
+        credentials: "include",
+      });
 
+      if (response.status === 200) {
+        router.push(`/`);
+        setErrorData("");
+      }
+    };
+    checkToken();
+  }, []);
+
+  const onSubmit = handleSubmit(async (data) => {
     setShowLoadingComponent(true);
+    try {
+      const response = await fetch(`${url}/auth/signup`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.status === 201) {
+        router.push("/");
+        setErrorData("");
+      }
+      if (response.status === 409) {
+        setErrorData("User already exists with this email");
+        setShowLoadingComponent(false);
+      }
+    } catch (error) {
+      console.error("error " + error);
+    }
   });
 
   return (
     <form className="w-full" onSubmit={onSubmit}>
       <legend className="sr-only">login</legend>
-      <fieldset className="flex flex-col gap-6">
+      <fieldset className="relative flex flex-col gap-6">
+        {errorData && (
+          <p className="HeadingSVariant absolute right-0 top-1 text-[#EC5757]">
+            {errorData}
+          </p>
+        )}
         <div className="relative flex flex-col gap-1">
           <InputForm
             placeholder="e.g. alex@email.com"
@@ -42,7 +85,7 @@ const SignupForm = () => {
           />
 
           {errors.email && (
-            <span className="absolute right-3 top-9">
+            <span className="absolute Body right-3 top-12 text-[#EC5757]">
               {errors.email.message}
             </span>
           )}
@@ -64,7 +107,7 @@ const SignupForm = () => {
           />
 
           {errors.password && (
-            <span className="absolute right-3 top-9">
+            <span className="absolute Body right-3 top-12 text-[#EC5757]">
               {errors.password.message}
             </span>
           )}
@@ -86,7 +129,7 @@ const SignupForm = () => {
           />
 
           {errors.confirm_password && (
-            <span className="absolute right-3 top-9">
+            <span className="absolute Body right-3 top-12 text-[#EC5757]">
               {errors.confirm_password.message}
             </span>
           )}
