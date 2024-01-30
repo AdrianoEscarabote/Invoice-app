4ae25eeeb3a45b2841c1fdd8790f76f820fb1f0b
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-01-30 13:42:21 -0300

    feat(form-elements): redoing the component logic

--- frontend/src/components/ItemList/ItemListProps.d.ts
+++ frontend/src/components/ItemList/ItemListProps.d.ts
@@ -1,6 +1,12 @@
-export interface ItemListProps {
-  items: ItemType[];
-}
+import {
+  Control,
+  FieldErrors,
+  UseFormGetValues,
+  UseFormRegister,
+  UseFormSetValue,
+} from "react-hook-form";
+import { CreateInvoiceData } from "../CreateInvoice/CreateInvoiceProps";
+import { ItemTypes } from "@/redux/redux-types";
 
 interface ItemType {
   name: string;
