fe78550c131ff03edbdba1a3bf8aeb43c51274ce
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-02-19 12:39:01 -0300

    chore(components): configuring and using redux in the storybook and test file

--- frontend/src/components/EditInvoice/EditInvoice.test.tsx
+++ frontend/src/components/EditInvoice/EditInvoice.test.tsx
@@ -1,9 +1,28 @@
 import { render } from "@testing-library/react";
 import EditInvoice from ".";
-import { InvoiceDataMock } from "@/utils/mock";
+import configureMockStore from "redux-mock-store";
+import getMockState from "@/utils/getMockState";
+import { Provider } from "react-redux";
+
+const mockStore = configureMockStore();
 
 describe("EditInvoice", () => {
+  let store;
+
+  beforeEach(() => {
+    const mockState = getMockState();
+    const state = mockStore(mockState);
+
+    store = state;
+  });
+
+  afterAll(() => jest.clearAllMocks());
+
   it("should render correctly", () => {
-    render(<EditInvoice data={InvoiceDataMock.data} />);
+    render(
+      <Provider store={store}>
+        <EditInvoice closeEditInvoice={() => {}} />
+      </Provider>
+    );
   });
 });
