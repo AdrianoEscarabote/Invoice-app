3bfdd5b8eef62d439b7b74dadbc14a78ee92d58e
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-02-23 16:02:12 -0300

    feat(components): adding mock hooks and context provider in test

--- frontend/src/components/Invoice/Invoice.test.tsx
+++ frontend/src/components/Invoice/Invoice.test.tsx
@@ -1,8 +1,48 @@
-import { render } from "@testing-library/react";
+import { RenderResult, render } from "@testing-library/react";
 import Invoice from ".";
+import configureMockStore from "redux-mock-store";
+import getMockState from "@/utils/getMockState";
+import { Provider } from "react-redux";
+import useUserAuthenticated from "@/hooks/useUserAuthenticated";
+import useInvoices from "@/hooks/useInvoices";
+
+const mockStore = configureMockStore();
+
+jest.mock("../../hooks/useUserAuthenticated", () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
+jest.mock("../../hooks/useInvoices", () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
 
 describe("InvoiceComponent", () => {
+  let store;
+  let wrapper: RenderResult;
+
+  beforeEach(async () => {
+    const mockState = getMockState();
+    const state = mockStore(mockState);
+
+    store = state;
+
+    (useUserAuthenticated as jest.Mock).mockImplementation(() => {});
+    (useInvoices as jest.Mock).mockImplementation(() => {});
+    wrapper = render(
+      <Provider store={store}>
+        <Invoice />
+      </Provider>
+    );
+  });
+
+  afterAll(() => jest.clearAllMocks());
+
   it("should render correctly", () => {
-    render(<Invoice invoiceCount={7} />);
+    render(
+      <Provider store={store}>
+        <Invoice />
+      </Provider>
+    );
   });
 });
