6f9687dedaf8e0fce7fd1703a3aa3d8ca3036c94
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-02-28 15:21:11 -0300

    chore(components): adding decorators on storybook & mocking state in unit test

--- frontend/src/components/ViewInvoice/ViewInvoice.stories.tsx
+++ frontend/src/components/ViewInvoice/ViewInvoice.stories.tsx
@@ -1,9 +1,20 @@
 import { Meta, StoryObj } from "@storybook/react";
 import ViewInvoice from ".";
+import { ViewInvoiceProps } from "./ViewInvoiceProps";
+import { Provider } from "react-redux";
+import store from "@/redux/store";
 
 export default {
   title: "components/ViewInvoice",
   component: ViewInvoice,
-} as Meta;
+  args: {
+    navigateToInvoice: () => {},
+  },
+  decorators: [
+    (Story) => {
+      return <Provider store={store}>{Story()}</Provider>;
+    },
+  ],
+} as Meta<ViewInvoiceProps>;
 
 export const Primary: StoryObj = {};
