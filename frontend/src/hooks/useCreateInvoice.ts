0dc6c541e5e34ec6498b2a2593a26f34f9e190dc
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-03-01 12:54:21 -0300

    chore(hooks): change prop paymentterms type

--- frontend/src/hooks/useCreateInvoice.ts
+++ frontend/src/hooks/useCreateInvoice.ts
@@ -11,7 +11,7 @@ interface useCreateInvoiceProps {
   clientName: string;
   clientEmail: string;
   description: string;
-  paymentTerms: "1" | "7" | "14" | "30";
+  paymentTerms: 1 | 7 | 14 | 30;
   status: "Pending" | "Paid" | "Draft";
   paymentDue: string;
   senderAddress: {
