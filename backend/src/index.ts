1d211074ba9b61edf7af811efabc326d05edb53e
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-03-04 15:09:38 -0300

    feat(backend): configuring cors and updating the url

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,59 @@
-import * as express from "express"
-import LoginRouter from "./routes/login"
+// env
+import { config } from "dotenv"
 
-const app = express()
+// database
+import { MongoClient } from "./database/mongo"
 
-app.use(LoginRouter)
+// express
+import express, { Request, Response, NextFunction } from "express"
 
-app.get("/home", (req, res) => {
-  res.status(200).send("estou no home")
-})
+// cookie-parser
+import cookieParser = require("cookie-parser")
 
-app.listen(4000, async () => {
-  console.log("listening on port 4000")
-})
+// cors
+import cors from "cors"
+
+// configures the cors to allow only one origin
+const corsOptions = {
+  credentials: true,
+  origin: "https://invoice-app-adrianoescarabote.vercel.app",
+}
+
+// routers
+import authRouter from "./routes/auth/auth"
+import InvoiceRouter from "./routes/invoice/invoice"
+
+const main = async () => {
+  config()
+
+  const app = express()
+
+  app.use(express.json())
+  app.use(cookieParser())
+  app.use(cors(corsOptions))
+  app.use(function (req: Request, res: Response, next: NextFunction) {
+    res.header(
+      "Access-Control-Allow-Origin",
+      "https://invoice-app-adrianoescarabote.vercel.app",
+    )
+    res.header("Access-Control-Allow-Credentials", "true")
+    next()
+  })
+
+  await MongoClient.connect()
+
+  app.get("/", (req: Request, res: Response) => {
+    res.send("Hello world!")
+  })
+
+  app.use("/auth", authRouter)
+  app.use("/invoice", InvoiceRouter)
+
+  const port = 5000
+
+  app.listen(port, () => {
+    console.log(`listening on port http://localhost:${port}`)
+  })
+}
+
+main()
